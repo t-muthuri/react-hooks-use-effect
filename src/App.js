1dbe362dbcdec919d91070bdbbcad941a074f077
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-10-04 10:35:59 +0300

    Completed assignment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+
+  useEffect(()=>{
+    document.title = text;
+  }, [text]);
+
+
   useEffect(() => {
-    console.log("useEffect called");
-  });
+    setTimeout(()=>setCount(0), 5000);
+  },[]);
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return (
+    <>
+      <button onClick={() => setCount((count) => count + 1)}>
+        I've been clicked {count} times
+      </button>
+      ;
+      <input 
+      type="text" 
+      placeholder="type away..." 
+      value={text}
+      onChange={(e)=>setText(e.target.value)}/>
+    </>
+  );
 }
-
 export default App;
